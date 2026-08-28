a2b62029a7c8ab686756a9ccbafb7fa02a40f99c
743218 <lifre5647@stu.pender.k12.nc.us>
2026-08-28 11:41:27 -0400

    Update Module.h

--- main/managers/modules/Module.h
+++ main/managers/modules/Module.h
@@ -4,13 +4,27 @@
 
 class Module {
 private:
-  std::string moduleName;
-  std::string moduleDescription;
-  int key;
+	std::string moduleName;
+	std::string moduleDescription;
+	int key;
+	bool enabled;
+
 public:
-  Module(std::string moduleName, ModuleCategory category, std::string description, int key = 0x0);
+	Module(std::string moduleName, ModuleCategory category, std::string description, int key = 0x0);
 	~Module();
 
-  virtual void onDirextXScreenOverlay();
-  virtual void onClientTick();
+	virtual void onDirextXScreenOverlay();
+	virtual void onClientTick();
+
+	static inline bool isEnabled() {
+		return enabled;
+	};
+
+	static inline void setEnabled(bool value) {
+		enabled = value;
+	}
+
+	static inline void toggle() {
+		enabled = !enabled;
+	}
 };
