d6f78992201f9c6c56dc76894ca58a3d98c0d900
743218 <lifre5647@stu.pender.k12.nc.us>
2026-08-28 10:16:58 -0400

    Create client.cpp

---
+++ main/client.cpp
@@ -0,0 +1,11 @@
+#include "client.h"
+
+// Created by 743218 Aug 28, 2026
+
+void Client::initialize() {
+  initialized = true;
+}
+
+void Client::clientMessage(std::string m) {
+  //We don't have memoryutils yet. Will be implemented later.
+}
