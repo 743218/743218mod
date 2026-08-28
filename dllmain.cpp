421311b309327fc6e362505f6b28b2c15e984948
743218 <lifre5647@stu.pender.k12.nc.us>
2026-08-28 10:26:04 -0400

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,1 +1,42 @@
+#include <Windows.h>
+#include "main/Client.h"
+#include "winrt/Windows.UI.Core.h"
+#include "winrt/windows.system.h"
+#include <winrt/Windows.UI.Notifications.h>
+#include <winrt/Windows.Data.Xml.Dom.h>
+#include "winrt/windows.applicationmodel.core.h"
+#include "winrt/Windows.UI.ViewManagement.h"
+#include "winrt/Windows.Foundation.h"
 
+void setWindowTitle(const std::wstring& title) {
+    winrt::Windows::ApplicationModel::Core::CoreApplication::MainView().CoreWindow().DispatcherQueue().TryEnqueue([title]() {
+        winrt::Windows::UI::ViewManagement::ApplicationView::GetForCurrentView().Title(title);
+        });
+}
+
+DWORD WINAPI initClient(LPVOID lpParameter) {
+    setWindowTitle(L"");
+
+    Client::init();
+
+    setWindowTitle(L"743218mod");
+
+    while (Client::isInitialized()) {
+        Sleep(50);
+    }
+
+    Sleep(25);
+
+    FreeLibraryAndExitThread((HMODULE)lpParameter, 1);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)initClient, hModule, 0, 0);
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
