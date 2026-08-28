ddc169558692ed0a009dc7de9ee27c13754eec23
743218 <lifre5647@stu.pender.k12.nc.us>
2026-08-28 11:20:06 -0400

    Update Module.h

--- main/managers/modules/Module.h
+++ main/managers/modules/Module.h
@@ -2,5 +2,14 @@
 #include "../../sdk/GameData.h"
 
 class Module {
+private:
+  std::string moduleName;
+  std::string moduleDescription;
+  int key;
+public:
+  Module(std::string moduleName, ModuleCategory category, std::string description, int key = 0x0);
+	~Module();
 
+  void onDirextXScreenOverlay();
+  void onClientTick();
 };
