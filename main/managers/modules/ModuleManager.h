a8bc1671c49b1a9f46790eae728d4ddebe2f0edc
743218 <lifre5647@stu.pender.k12.nc.us>
2026-08-28 11:34:11 -0400

    Update ModuleManager.h

--- main/managers/modules/ModuleManager.h
+++ main/managers/modules/ModuleManager.h
@@ -2,9 +2,11 @@
 #include "Module.h"
 
 class ModuleManager {
+private:
+	bool initialized = false;
 public:
 	static inline std::vector<Module*> moduleList;
 
-  static void initialize();
-  static void shutDown();
+	static void initialize();
+	static void shutDown();
 };
